064aeb2651f14366254bd8630825c26be291229d
Alexandr_Ovchar <azalexxx02@gmail.com>
2023-04-21 21:43:05 +0300

    arrays

--- main.c
+++ main.c
@@ -3,20 +3,8 @@
 
 int main()
 {
-    char color[20];
-    char pluarNoun[20];
-    char celebrity[20];
-
-    printf("enter a Color: ");
-    scanf("%s", color);
-    printf("enter a plural noun: ");
-    scanf("%s", pluarNoun);
-    printf("enter a celebrity: ");
-    scanf("%s", celebrity);
-
-    printf("roses ere %s\n", color);
-    printf("%s are blue\n", pluarNoun);
-    printf("i love %s\n", celebrity);
+    int lucky[] = {1, 2, 3, 4, 5, 6, 7};
+    printf("%d", lucky[1]);
 
     return 0;
 }
