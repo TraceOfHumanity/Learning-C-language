199ce9e08cf246f766be897885707db08ee120cf
Alexandr_Ovchar <azalexxx02@gmail.com>
2023-04-29 21:24:57 +0300

    functions

--- main.c
+++ main.c
@@ -3,8 +3,39 @@
 
 int main()
 {
-    int lucky[] = {1, 2, 3, 4, 5, 6, 7};
-    printf("%d", lucky[1]);
+
+    // int age;
+    // printf("Enter Your age: ");
+    // scanf("%d", &age);
+    // printf("You are %d years old \n", age);
+
+    // double gpa;
+    // printf("Enter Your gpa: ");
+    // scanf("%lf", &gpa);
+    // printf("Your gpa is %f \n", gpa);
+
+    // char grade;
+    // printf("Enter Your grade: ");
+    // scanf("%c", &grade);
+    // printf("Your grade is %c \n", grade);
+
+    // char name[20];
+    // printf("Enter Your name: ");
+    // scanf("%s", name);
+    // printf("Your name is %s \n", name);
+
+    // char name[20];
+    // printf("Enter Your name: ");
+    // fgets(name, 20, stdin);
+    // printf("Your name is %s \n", name);
+
+    sayHi("Mike", 40);
+    sayHi("Tom", 20);
+    sayHi("Oscar", 50);
 
     return 0;
+}
+void sayHi(char name[], int age)
+{
+    printf("hello %s, you are %d\n", name, age);
 }
