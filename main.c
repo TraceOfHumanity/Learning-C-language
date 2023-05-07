76491938349e4b202183a92cf7de8d9d9310298d
Alexandr_Ovchar <azalexxx02@gmail.com>
2023-05-07 19:34:46 +0300

    if

--- main.c
+++ main.c
@@ -1,41 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int max(int num1, int num2, int num3)
+{
+    int result;
+    if (num1 >= num2 && num1 >= num3)
+    {
+        result = num1;
+    }
+    else if (num2 >= num1 && num2 >= num3)
+    {
+        result = num2;
+    }
+    else
+    {
+        result = num3;
+    }
+    return result;
+}
+
 int main()
 {
-
-    // int age;
-    // printf("Enter Your age: ");
-    // scanf("%d", &age);
-    // printf("You are %d years old \n", age);
-
-    // double gpa;
-    // printf("Enter Your gpa: ");
-    // scanf("%lf", &gpa);
-    // printf("Your gpa is %f \n", gpa);
-
-    // char grade;
-    // printf("Enter Your grade: ");
-    // scanf("%c", &grade);
-    // printf("Your grade is %c \n", grade);
-
-    // char name[20];
-    // printf("Enter Your name: ");
-    // scanf("%s", name);
-    // printf("Your name is %s \n", name);
-
-    // char name[20];
-    // printf("Enter Your name: ");
-    // fgets(name, 20, stdin);
-    // printf("Your name is %s \n", name);
-
-    sayHi("Mike", 40);
-    sayHi("Tom", 20);
-    sayHi("Oscar", 50);
-
+    printf("%d", max(400, 100, 1000));
     return 0;
-}
-void sayHi(char name[], int age)
-{
-    printf("hello %s, you are %d\n", name, age);
 }
