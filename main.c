8c1b16740777eaa622c713e38b36a450a9cd5e13
Alexandr_Ovchar <azalexxx02@gmail.com>
2023-05-25 18:53:41 +0300

    switchCase

--- main.c
+++ main.c
@@ -3,36 +3,30 @@
 
 int main()
 {
-    double num1;
-    double num2;
-    char op;
+    char grade = 'A';
 
-    printf("enter a number: ");
-    scanf("%lf", &num1);
-    printf("enter operator: ");
-    scanf(" %c", &op);
-    printf("enter a number: ");
-    scanf("%lf", &num2);
-
-    if (op == '+')
+    switch (grade)
     {
-        printf("%f", num1 + num2);
-    }
-    else if (op == '-')
-    {
-        printf("%f", num1 - num2);
-    }
-    else if (op == '/')
-    {
-        printf("%f", num1 / num2);
-    }
-    else if (op == '*')
-    {
-        printf("%f", num1 * num2);
-    }
-    else
-    {
-        printf("invalid opetator");
+    case 'A':
+        printf("aaa");
+        break;
+    case 'B':
+        printf("bbb");
+        break;
+    case 'C':
+        printf("ccc");
+        break;
+    case 'D':
+        printf("ddd");
+        break;
+    case 'E':
+        printf("eee");
+        break;
+    case 'F':
+        printf("fff");
+        break;
+    default:
+        printf("default");
     }
 
     return 0;
