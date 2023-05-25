729a951feb7121b0af1184b3f2ecb53c5495135b
Alexandr_Ovchar <azalexxx02@gmail.com>
2023-05-25 19:49:45 +0300

    conditionalStatements

---
+++ conditionalStatements/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 40;
+
+    if (a > 10)
+    {
+        printf("www");
+        if (a > 20)
+        {
+
+            printf("123");
+        }
+    } else printf("uuuuuuu");
+
+    return 2;
+}
