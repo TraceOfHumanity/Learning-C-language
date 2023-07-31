c1a11756e889ecd423df075152ce20dd7a6f3ce2
Alexander_Ovchar <99219332+TraceOfHumanity@users.noreply.github.com>
2023-07-31 12:08:47 +0300

    Update pong.c

--- pong.c
+++ pong.c
@@ -1,33 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
 
 #define width 65
 #define height 25
 
 char mas[height][width + 1];
 
-void init() {
-	for (int i = 0; i < width - 1; i++) {
+typedef struct {
+	int x, y;
+	int w;
+}TRacket;
+TRacket racket;
+
+
+void initRacket() {
+	racket.w = 7;
+	racket.x = (width - racket.w) / 2;
+	racket.y = height - 1;
+}
+
+void putRacket() {
+	for (int i = racket.x; i < racket.x + racket.w; i++)
+		mas[racket.y][i] = '=';
+}
+
+void init()
+{
+	for (int i = 0; i < width; i++)
 		mas[0][i] = '#';
-	}
-	mas[0][width - 1] = '\0'; // Закриваємо перший рядок нуль-термінатором
+	mas[0][width] = '\0';
 
-	for (int i = 1; i < height - 1; i++) {
-		strncpy(mas[i], mas[0], width - 1); // Копіюємо всі символи рядка, але без нуль-термінатора
-		mas[i][width - 1] = '\0'; // Кожен рядок має закінчуватись нуль-термінатором
-	}
+	strncpy(mas[1], mas[0], width + 1);
+	for (int i = 1; i < width - 1; i++)
+		mas[1][i] = ' ';
+
+	for (int i = 2; i < height; i++)
+		strncpy(mas[i], mas[1], width + 1);
 }
 
-void show() {
+void moveRacket(int x) {
+	racket.x = x;
+	if (racket.x < 1)
+		racket.x = 1;
+	if (racket.x + racket.w > width - 2)
+		racket.x = width - 2 - racket.w;
+}
+
+void show()
+{
 	for (int i = 0; i < height; i++) {
+
 		printf("%s\n", mas[i]);
+		if (i > height - 1)
+			printf("\n");
 	}
 }
 
-int main() {
-	init();
-	show();
+void setCursor(int x, int y)
+{
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+int main()
+{
+	initRacket();
+
+	do {
+		setCursor(0, 0);
+		init();
+		putRacket();
+		show();
+
+		if (GetKeyState('A') < 0)
+			moveRacket(racket.x - 2);
+		if (GetKeyState('D') < 0)
+			moveRacket(racket.x + 2);
+		Sleep(10);
+	} while (GetKeyState(VK_ESCAPE) >= 0);
 
 	return 0;
 }
